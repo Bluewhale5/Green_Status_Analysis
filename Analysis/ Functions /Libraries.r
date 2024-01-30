27c7325b65304d509011ac865b2c08d2c4e77885
Bluewhale5 <117742839+Bluewhale5@users.noreply.github.com>
2024-01-30 10:34:48 +0000

    Update Libraries.r

--- Analysis/ Functions /Libraries.r
+++ Analysis/ Functions /Libraries.r
@@ -1,6 +1,6 @@
 #Libraries required for preparation, analysis, and presentation of the data
 
-library(dplyr)
+suppressPackageStartupMessages((library(dplyr))
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
